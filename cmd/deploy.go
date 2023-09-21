4e6e7c16e2f647914c9967f54e384577f91891b0
王安楠 <7938095+wangannan0913@user.noreply.gitee.com>
2023-09-21 23:40:13 +0800

    improve deploy update

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -7,7 +7,7 @@ import (
 )
 
 var interDeploy *deploy.Deploy
-var ListenAddr string
+var flagParams = &deploy.FlagParams{}
 
 // rootCmd represents the base command when called without any subcommands
 var deployCmd = &cobra.Command{
@@ -17,12 +17,13 @@ var deployCmd = &cobra.Command{
 usage: xuanwu-agent deploy --listen 0.0.0.0:8000 
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		os.Exit(interDeploy.Listen(ListenAddr))
+		os.Exit(interDeploy.Listen(flagParams))
 	},
 }
 
 func init() {
 	interDeploy = deploy.NewDeploy()
 
-	deployCmd.Flags().StringVarP(&ListenAddr, "address", "a", "127.0.0.1:8080", "The Address and Port which deploy listen at")
+	deployCmd.Flags().StringVarP(&flagParams.Address, "address", "a", "127.0.0.1:8080", "The Address and Port which deploy listen at")
+	deployCmd.Flags().BoolVar(&flagParams.InCluster, "in-cluster", false, "Specify the xuanwu-agent is deploy in a kubernetes cluster")
 }
--- internal/deploy/deploy.go
+++ internal/deploy/deploy.go
@@ -1,14 +1,18 @@
 package deploy
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"xuanwu-agent/internal/base"
 	"xuanwu-agent/pkg/consolelog"
 )
 
 type Deploy struct {
 	*base.Base
-	cLog *consolelog.ConsoleLog
+	cLog       *consolelog.ConsoleLog
+	kubeClient kubernetes.Interface
 }
 
 func NewDeploy() *Deploy {
@@ -18,14 +22,57 @@ func NewDeploy() *Deploy {
 	}
 }
 
-func (d *Deploy) Listen(addr string) int {
+type FlagParams struct {
+	Address   string `json:"address"`
+	InCluster bool   `json:"inCluster"`
+}
+
+func (d *Deploy) Listen(flagParams *FlagParams) int {
+	err := d.init(flagParams)
+	if err != nil {
+		d.cLog.Errorf("init deploy failed: %v", err)
+		return base.ReturnError
+	}
+
 	r := gin.Default()
 	r.GET("/deploy/update", d.HandlerUpdate)
 
 	for {
-		err := r.Run(addr)
+		err := r.Run(flagParams.Address)
 		if err != nil {
 			d.cLog.Error(err.Error())
 		}
 	}
+}
+
+func (d *Deploy) init(flagParams *FlagParams) error {
+	if d.cLog == nil {
+		d.cLog = consolelog.NewConsoleLog()
+	}
+
+	if flagParams.InCluster {
+		err := d.newInClusterKubeClient()
+		if err != nil {
+			return err
+		}
+	} else {
+		return errors.New("currently, the deploy module of xuanwu-agent is only supported to deployed into a kubernetes cluster")
+	}
+
+	return nil
+}
+
+func (d *Deploy) newInClusterKubeClient() error {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return err
+	}
+
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	d.kubeClient = clientSet
+	return nil
 }
