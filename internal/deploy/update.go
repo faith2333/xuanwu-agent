e3ae801d6e21f3b98fce72b8d317cfdb6a991911
王安楠 <7938095+wangannan0913@user.noreply.gitee.com>
2023-09-22 11:06:45 +0800

    improve update

--- internal/deploy/update.go
+++ internal/deploy/update.go
@@ -1,11 +1,13 @@
 package deploy
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type UpdateParams struct {
@@ -36,6 +38,12 @@ func (d *Deploy) HandlerUpdate(c *gin.Context) {
 		return
 	}
 
+	err = d.update(params)
+	if err != nil {
+		d.HttpResponseFailed(c, err.Error())
+		return
+	}
+
 	d.HttpResponseSuccess(c, "Update Success")
 }
 
@@ -46,6 +54,39 @@ func (d *Deploy) validateUpdateParams(params *UpdateParams) error {
 
 	if params.Image == "" {
 		return errors.New(fmt.Sprintf("Image can not be empty"))
+	}
+
+	if params.Namespace == "" {
+		return errors.New("namespace can not be empty")
+	}
+
+	if params.ResourceName == "" {
+		return errors.New("resource name can not be empty")
+	}
+
+	return nil
+}
+
+func (d *Deploy) update(param *UpdateParams) error {
+	switch param.ResourceType.Upper() {
+	case ResourceTypeDeployment:
+		return d.updateDeployment(param)
+	}
+	return errors.New(fmt.Sprintf("resource type %s has not been supported", param.ResourceType))
+}
+
+func (d *Deploy) updateDeployment(params *UpdateParams) error {
+	ctx := context.Background()
+	deployment, err := d.kubeClient.AppsV1().Deployments(params.Namespace).Get(ctx, params.ResourceName, metav1.GetOptions{})
+	if err != nil {
+		return errors.New(fmt.Sprintf("get the deployment %q in namespace %q failed: %v", params.ResourceName, params.Namespace, err))
+	}
+
+	deployment.Spec.Template.Spec.Containers[0].Image = params.Image
+
+	_, err = d.kubeClient.AppsV1().Deployments(params.Namespace).Update(ctx, deployment, metav1.UpdateOptions{})
+	if err != nil {
+		return err
 	}
 
 	return nil
